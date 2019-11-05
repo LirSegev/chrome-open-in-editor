c54f3a13132823aaed11c108eebaef7f1ef78b8f
Lir Segev <lir.segev@gmail.com>
2019-11-05 15:08:17 +0100

    Fix regex expression
    Create constant ACTIVE_TIME

--- ts/devtools.ts
+++ ts/devtools.ts
@@ -1,3 +1,8 @@
+/**
+ * Time in seconds for the listener to remain active after calling activate()
+ */
+const ACTIVE_TIME = 1.5;
+
 let timeoutId: any = undefined;
 
 function onSelectionChanged(
@@ -11,7 +16,7 @@ function onSelectionChanged(
 ) {
 	const { url, startLine: line, startColumn: column } = e;
 	if (url) {
-		const regexResults = url.match('/(?<=//.+/).*/');
+		const regexResults = url.match('(?<=//.+/).*');
 		if (regexResults) {
 			const filePath = regexResults[0];
 			const callOpenFile = () => openFile(filePath, line, column);
@@ -38,19 +43,19 @@ function openFile(path: string, line?: number, column?: number) {
 }
 
 function deactivate() {
-	console.log('deactivate');
+	// console.log('deactivate');
 	chrome.devtools.panels.sources.onSelectionChanged.removeListener(
 		onSelectionChanged as () => void
 	);
 }
 
 function activate() {
-	console.log('activate');
+	// console.log('activate');
 	chrome.devtools.panels.sources.onSelectionChanged.addListener(
 		onSelectionChanged as () => void
 	);
 
-	setTimeout(deactivate, 1.5 * 1000);
+	setTimeout(deactivate, ACTIVE_TIME * 1000);
 }
 
 chrome.commands.onCommand.addListener(command => {
